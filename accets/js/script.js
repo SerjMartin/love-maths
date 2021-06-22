beeaaeb9a70cf679f742393d7622674d253b8735
SerjMartin <serjmartin@mail.com>
2021-06-22 19:07:41 +0000

    Add event listenr for document loading and button click

--- accets/js/script.js
+++ accets/js/script.js
@@ -1,3 +1,23 @@
+// wait for the DOM to finishloading before running the game
+// Get the button elementa and event listeners to them
+
+document.addEventListener("DOMContentLoaded", function() {
+   let buttons = document.getElementsByTagName("button");
+   // when this button is click then event from inside from this button will run
+   for (let button of buttons) {
+        button.addEventListener("click", function() {
+            //check the value for attribute with the name data-type
+            // (this) using for the specific button, in this case is submit button
+         if (this.getAttribute("data-type") === "submit") {
+             alert("You clicked Submit!");
+         } else {
+             let gameType = this.getAttribute("data-type"); // (this) mean the button what will be click with atribute data-type
+             alert(`You clicked ${gameType}`); // this tells the user what button has been clicked
+         }
+        })
+   }
+})
+
 function runGame() {
 
 }
@@ -27,5 +47,5 @@ function displaySubstractQuestion() {
 }
 
 function dislayMultiplyQuestion() {
-    
+
 }
